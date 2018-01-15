56d70cc64cc398c633ee912d8d5a69ff1843288f
Grubeo <robben_1@wp.pl>
2018-01-15 23:34:13 +0100

    Fixed error and adding make_signed metafunction
    Fixed error: type_if instead of type_is

--- type_traits.hpp
+++ type_traits.hpp
@@ -16,16 +16,25 @@ namespace llstd
     using type_if_t = typename type_if<B, T, F>::type;
 
     template<typename T>
-    struct make_unsigned { using type = T; };
+    struct make_unsigned : type_is<T> {};
 
-    template<> struct make_unsigned<signed char> : type_if<unsigned char> {};
-    template<> struct make_unsigned<signed short> : type_if<unsigned short> {};
-    template<> struct make_unsigned<signed int> : type_if<unsigned int> {};
-    template<> struct make_unsigned<signed long> : type_if<unsigned long> {};
-    template<> struct make_unsigned<signed long long> : type_if<unsigned long long> {};
+    template<> struct make_unsigned<signed char> : type_is<unsigned char> {};
+    template<> struct make_unsigned<signed short> : type_is<unsigned short> {};
+    template<> struct make_unsigned<signed int> : type_is<unsigned int> {};
+    template<> struct make_unsigned<signed long> : type_is<unsigned long> {};
+    template<> struct make_unsigned<signed long long> : type_is<unsigned long long> {};
 
     template<typename T>
     using make_unsigned_t = typename make_unsigned<T>::type;
+    
+    template<typename T>
+    struct make_signed : type_is<T> {};
+    
+    template<> struct make_signed<unsigned char> : type_is<signed char> {};
+    template<> struct make_signed<unsigned short> : type_is<signed short> {};
+    template<> struct make_signed<unsigned int> : type_is<signed int> {};
+    template<> struct make_signed<unsigned long> : type_is<signed long> {};
+    template<> struct make_signed<unsigned long long> : type_is<signed long long> {};
 }
 
 #endif
