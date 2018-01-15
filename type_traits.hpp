8b6ff007d992f272855d4bd3f110c1b6295a2cbb
Grubeo <robben_1@wp.pl>
2018-01-15 23:26:50 +0100

    Changing naming convention again

--- type_traits.hpp
+++ type_traits.hpp
@@ -4,25 +4,25 @@
 namespace llstd
 {
     template<typename T>
-    struct type { using type = T; };
+    struct type_is { using type = T; };
     
     template<bool B, typename, typename F>
-    struct type_if : type<T> {};
+    struct type_if : type_is<T> {};
 
     template<typename T, typename F>
-    struct type_if<true, T, F> : type<T> {};
+    struct type_if<true, T, F> : type_is<T> {};
     
     template<bool B, typename T, typename F>
-    using type_if_t = typename conditional<B, T, F>::type;
+    using type_if_t = typename type_if<B, T, F>::type;
 
     template<typename T>
     struct make_unsigned { using type = T; };
 
-    template<> struct make_unsigned<signed char> : type<unsigned char> {};
-    template<> struct make_unsigned<signed short> : type<unsigned short> {};
-    template<> struct make_unsigned<signed int> : type<unsigned int> {};
-    template<> struct make_unsigned<signed long> : type<unsigned long> {};
-    template<> struct make_unsigned<signed long long> : type<unsigned long long> {};
+    template<> struct make_unsigned<signed char> : type_if<unsigned char> {};
+    template<> struct make_unsigned<signed short> : type_if<unsigned short> {};
+    template<> struct make_unsigned<signed int> : type_if<unsigned int> {};
+    template<> struct make_unsigned<signed long> : type_if<unsigned long> {};
+    template<> struct make_unsigned<signed long long> : type_if<unsigned long long> {};
 
     template<typename T>
     using make_unsigned_t = typename make_unsigned<T>::type;
