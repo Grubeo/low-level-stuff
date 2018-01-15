ad77634046e3077abd47e4bb19db5d0e7df3aa9d
Grubeo <robben_1@wp.pl>
2018-01-15 13:58:54 +0100

    Changing type_traits implementation

--- type_traits.hpp
+++ type_traits.hpp
@@ -1,25 +1,28 @@
 #ifndef _TYPE_TRAITS_HPP
 #define _TYPE_TRAITS_HPP
 
-namespace std
+namespace llstd
 {
+    template<typename T>
+    struct type { using type = T; };
+    
     template<bool B, typename, typename F>
-    struct conditional { using type = F; };
+    struct type_if : type<T> {};
 
     template<typename T, typename F>
-    struct conditional<true, T, F> { using type = T; };
-
+    struct type_if<true, T, F> : type<T> {};
+    
     template<bool B, typename T, typename F>
-    using conditional_t = typename conditional<B, T, F>::type;
+    using type_if_t = typename conditional<B, T, F>::type;
 
     template<typename T>
     struct make_unsigned { using type = T; };
 
-    template<> struct make_unsigned<signed char> { using type = unsigned char; };
-    template<> struct make_unsigned<signed short> { using type = unsigned short; };
-    template<> struct make_unsigned<signed int> { using type = unsigned int; };
-    template<> struct make_unsigned<signed long> { using type = unsigned long; };
-    template<> struct make_unsigned<signed long long> { using type = unsigned long long; };
+    template<> struct make_unsigned<signed char> : type<unsigned char> {};
+    template<> struct make_unsigned<signed short> : type<unsigned short> {};
+    template<> struct make_unsigned<signed int> : type<unsigned int> {};
+    template<> struct make_unsigned<signed long> : type<unsigned long> {};
+    template<> struct make_unsigned<signed long long> : type<unsigned long long> {};
 
     template<typename T>
     using make_unsigned_t = typename make_unsigned<T>::type;
