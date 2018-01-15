33970f6a84cfcf0499a76cbf4f76610c911eb6b8
Grubeo <robben_1@wp.pl>
2018-01-15 13:53:18 +0100

    Adding hardware port API (8,16 and 32 bit)

---
+++ port.hpp
@@ -0,0 +1,35 @@
+#include "fixed_ints.hpp"
+
+template<unsigned PORTSIZE>
+struct Port {
+    std::uint16_t port;
+    
+    explicit Port(std::uint16_t port)
+        : port(port) {}
+
+    using value_type = details::fixed_width_unsigned_t<PORTSIZE>;
+    
+    void write(value_type value) {
+        if constexpr (PORTSIZE == 8)
+            asm volatile ("outb %0, %1" : : "a"(value), "Nd"(port));
+        else if constexpr (PORTSIZE == 16)
+            asm volatile ("outw %0, %1" : : "a"(value), "Nd"(port));
+        else if constexpr (PORTSIZE == 32)
+            asm volatile ("outl %0, %1" : : "a"(value), "Nd"(port));
+        else 
+            asm ("ud2");
+    }
+
+    value_type read() {
+        value_type retval;
+
+        if constexpr (PORTSIZE == 8)
+            asm volatile ("inb %1, %0" : : "=a"(retval), "Nd"(port));
+        else if constexpr (PORTSIZE == 16)
+            asm volatile ("inw %1, %0" : : "=a"(retval), "Nd"(port));
+        else if constexpr (PORTSIZE == 32)
+            asm volatile ("inl %1, %0" : : "a"(retval), "Nd"(port));
+        else 
+            asm ("ud2");
+    }
+};
