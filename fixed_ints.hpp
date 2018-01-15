48a04581e897ae68f98bb7fcaa1a9f6378720cac
Grubeo <robben_1@wp.pl>
2018-01-15 14:03:33 +0100

    Updating naming convention

--- fixed_ints.hpp
+++ fixed_ints.hpp
@@ -3,17 +3,17 @@
 
 #include "type_traits.hpp"
 
-namespace details
+namespace llstd
 {
 	template<unsigned SIZE>
 	struct fixed_width_signed
 	{
 		static constexpr unsigned byte_size = SIZE / 8;
 
-		using type = std::conditional_t<byte_size == sizeof(signed char), char,
-			std::conditional_t<byte_size == sizeof(short), short,
-				std::conditional_t<byte_size == sizeof(int), int,
-					std::conditional_t<byte_size == sizeof(long), long,
+		using type = llstd::type_if_t<byte_size == sizeof(signed char), char,
+			llstd::type_if_t<byte_size == sizeof(short), short,
+				llstd::type_if_t<byte_size == sizeof(int), int,
+					llstd::type_if_t<byte_size == sizeof(long), long,
 						long long
 					>
 				>
@@ -32,16 +32,16 @@ namespace details
 
 	template<unsigned SIZE>
 	using fixed_width_unsigned_t = typename fixed_width_unsigned<SIZE>::type;
+	
+	using int8_t  = utils::fixed_width_signed_t<8>;
+	using int16_t = utils::fixed_width_signed_t<16>;
+	using int32_t = utils::fixed_width_signed_t<32>;
+	using int64_t = utils::fixed_width_signed_t<64>;
+
+	using uint8_t  = utils::fixed_width_unsigned_t<8>;
+	using uint16_t = utils::fixed_width_unsigned_t<16>;
+	using uint32_t = utils::fixed_width_unsigned_t<32>;
+	using uint64_t = utils::fixed_width_unsigned_t<64>;
 }
-
-using int8_t  = utils::fixed_width_signed_t<8>;
-using int16_t = utils::fixed_width_signed_t<16>;
-using int32_t = utils::fixed_width_signed_t<32>;
-using int64_t = utils::fixed_width_signed_t<64>;
-
-using uint8_t  = utils::fixed_width_unsigned_t<8>;
-using uint16_t = utils::fixed_width_unsigned_t<16>;
-using uint32_t = utils::fixed_width_unsigned_t<32>;
-using uint64_t = utils::fixed_width_unsigned_t<64>;
 
 #endif
