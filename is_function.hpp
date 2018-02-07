833aa7290344525b4e1b5f677887d9443110f827
Grubeo <robben_1@wp.pl>
2018-02-07 23:42:57 +0100

    Update is_function.hpp

--- is_function.hpp
+++ is_function.hpp
@@ -1,15 +1,9 @@
 template<class>
 struct is_function : llstd::false_type { };
- 
-// specialization for regular functions
 template<class Ret, class... Args>
 struct is_function<Ret(Args...)> : llstd::true_type {};
- 
-// specialization for variadic functions such as llstd::printf
 template<class Ret, class... Args>
 struct is_function<Ret(Args......)> : llstd::true_type {};
- 
-// specialization for function types that have cv-qualifiers
 template<class Ret, class... Args>
 struct is_function<Ret(Args...) const> : llstd::true_type {};
 template<class Ret, class... Args>
@@ -22,8 +16,6 @@ template<class Ret, class... Args>
 struct is_function<Ret(Args......) volatile> : llstd::true_type {};
 template<class Ret, class... Args>
 struct is_function<Ret(Args......) const volatile> : llstd::true_type {};
- 
-// specialization for function types that have ref-qualifiers
 template<class Ret, class... Args>
 struct is_function<Ret(Args...) &> : llstd::true_type {};
 template<class Ret, class... Args>
@@ -56,9 +48,6 @@ template<class Ret, class... Args>
 struct is_function<Ret(Args......) volatile &&> : llstd::true_type {};
 template<class Ret, class... Args>
 struct is_function<Ret(Args......) const volatile &&> : llstd::true_type {};
- 
-// specializations for noexcept versions of all the above (C++17 and later)
- 
 template<class Ret, class... Args>
 struct is_function<Ret(Args...) noexcept> : llstd::true_type {};
 template<class Ret, class... Args>
